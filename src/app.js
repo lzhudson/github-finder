0365ffcbaef95df5f6f4af5723288102ff72022f
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-19 11:46:25 -0300

    style: remove comments

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
-// Search input
 const searchUser = document.getElementById('searchUser');
 
-// Init Github
 const github = new Github();
 
-// Init UI
 const ui = new UI();
 
-// Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-  // Get input text
   const userText = e.target.value;
 
  
@@ -17,17 +12,13 @@ searchUser.addEventListener('keyup', (e) => {
     github.getUser(userText)
           .then(data => { 
             if(data.profile.message === 'Not Found') {
-              // Show alert
               ui.showAlert('User not found', 'alert alert-danger');
             } else {
-              // Show profile
               ui.showProfile(data.profile);
-              // Show repos
               ui.showRepos(data.repos);
             }
           })
   } else {
-    // Clear profile  
     ui.clearProfile();
   }
 })
