940a3bdd995dcf450dd7b989f35a6e0338de9ac7
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-19 12:27:52 -0300

    refact: update entry points and filename output

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 /** @type {import('webpack').Configuration} */
 
 module.exports = {
-  entry: {
-    app: path.resolve(__dirname, 'src', 'app.js'),
-    ui: path.resolve(__dirname, 'src', 'ui.js'),
-    github: path.resolve(__dirname, 'src', 'github.js') 
-  },
+  entry: [
+    path.resolve(__dirname, 'src', 'app.js'),
+    path.resolve(__dirname, 'src', 'ui.js'),
+    path.resolve(__dirname, 'src', 'github.js') 
+  ],
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].js'
+    filename: 'bundle.js'
   },
   devServer: {
     contentBase: path.join(__dirname, 'public'),
