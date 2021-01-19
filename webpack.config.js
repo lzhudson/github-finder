0ada38b43f64ca22238d88c2fea08c7d2cbbe144
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-19 11:26:58 -0300

    chore: create and add initial config webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+/** @type {import('webpack').Configuration} */
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'js', 'app.js'),
+    ui: path.resolve(__dirname, 'js', 'ui.js'),
+    github: path.resolve(__dirname, 'js', 'github.js') 
+  },
+  output: {
+    path: path.resolve(__dirname, 'bunlded'),
+    filename: '[name].js'
+  }
+}
